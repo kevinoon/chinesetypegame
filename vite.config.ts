b4021c6ce03bf449dffef780729fc3268a06aa50
kevinoon <ripplitt@protonmail.com>
2026-03-24 08:52:40 -0700

    Please work

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+<<<<<<< HEAD
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+=======
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+>>>>>>> 2a1b081 (Please work)
 })
